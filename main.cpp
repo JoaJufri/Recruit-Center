561283ef9b25ff8724e1cca0b7e4bd3d0b611427
JoaJufri <joaquinjufrida@gmail.com>
2022-11-09 21:30:44 -0300

    se limpia el main

--- main.cpp
+++ main.cpp
@@ -4,8 +4,7 @@ using namespace std;
 #include "funciones.h"
 #include "rlutil.h"
 
-#include "ArchivoCandidatos.h"
-#include "ArchivoBusquedas.h"
+
 
 int main()
 {
@@ -13,51 +12,10 @@ int main()
     rlutil::cls();
 
 
-    //Carga Recruiters
-    /*
-    ArchivoRecruiter archivo;
-    Recruiter recruiter;
-
-    recruiter.Cargar();
-    archivo.guardar(recruiter);
-    rlutil::cls();
-    */
-    /////////////////////////Carga Candidatos/////////////////////////////////////
-
-
-    ArchivoCandidatos archivo;
-    Candidato candidato;
-
-/*
-    char otro='S';
-    while(toupper(otro)=='S'){
-        AltaCandidato();
-        int tam = archivo.getCantidad();
-        candidato = archivo.leer(tam-1);
-        candidato.Mostrar();
-        cout<<endl<<"Otro ? S/N : ";
-        cin>>otro;
-    }
-
-*/
-
-    ///MOSTRAR CANDIDATOS CARGADOS HASTA AHORA
-
-    int tam = archivo.getCantidad();
-    cout<<"CARGADOS: "<<tam<<endl;
-    system("pause");
-    system("cls");
-    for(int i=0; i<tam; i++){
-        candidato=archivo.leer(i);
-        candidato.Mostrar();
-    }
-
-
-
     ///INICIO PROGRAMA
 
-    //PantallaInicio();
-    //MenuPpal();
+    PantallaInicio();
+    MenuPpal();
 
 
 }
